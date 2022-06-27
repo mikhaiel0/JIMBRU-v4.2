bb28c1c2a62ebe24067caf90fc0f264904ce55bd
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2022-06-27 17:36:49 +0530

    Create Video.js

---
+++ Video.js
@@ -0,0 +1,111 @@
+//════════════════════════════//
+
+//════════════════════════════//
+
+process.on('uncaughtException', console.error) //Error log
+require("./config")
+const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType, WAFlag } = require('@adiwajshing/baileys')
+const zJimbruOffical = require("@adiwajshing/baileys")
+
+
+
+
+
+
+const { smsg, formatp, tanggal, formatDate, getTime, isUrl, sleep, clockString, runtime, fetchJson, getBuffer, jsonformat, format, parseMention, getRandom } = require('./lib/myfunc')
+
+const cheerio = require ("cheerio");
+
+const { detikNews } = require('./lib/detik')
+
+const { Gempa } = require("./lib/gempa.js");
+const ms = require('ms')
+ let { covid } = require('./lib/covid.js') 
+const { jadwaltv }= require('./lib/jadwaltv');
+const maker = require('mumaker')
+const xeonkey = require('xfarr-api')
+const { hentai } = require('./lib/scraper2.js')
+let { msgFilter } = require('./lib/antispam')
+
+const {
+ FajarNews, 
+ BBCNews,
+  metroNews,
+  CNNNews,
+  iNews,
+  KumparanNews,
+  TribunNews,
+  DailyNews,
+  DetikNews,
+  OkezoneNews,
+  CNBCNews,
+  KompasNews,
+  SindoNews,
+  TempoNews,
+  IndozoneNews,
+  AntaraNews,
+  RepublikaNews,
+  VivaNews,
+  KontanNews,
+  MerdekaNews,
+  KomikuSearch,
+  AniPlanetSearch,
+  KomikFoxSearch,
+  KomikStationSearch,
+  MangakuSearch,
+  KiryuuSearch,
+  KissMangaSearch,
+  KlikMangaSearch,
+  PalingMurah,
+  LayarKaca21,
+  AminoApps,
+  Mangatoon,
+  WAModsSearch,
+  Emojis,
+  CoronaInfo,
+  JalanTikusMeme, 
+  Cerpen,
+  Quotes,
+  Couples,
+  Darkjokes
+} = require("dhn-api");
+const _ = require('lodash')
+const yargs = require('yargs/yargs')
+var low
+try {
+  low = require('lowdb')
+} catch (e) {
+  low = require('./lib/lowdb')
+}
+
+const { Low, JSONFile } = low
+const mongoDB = require('./lib/mongoDB')
+const { 
+  yta, 
+  ytv, 
+  searchResult 
+ } = require('./lib/ytdl')
+ //database other
+let banUser = JSON.parse(fs.readFileSync('./database/banUser.json'));
+let banchat = JSON.parse(fs.readFileSync('./database/banChat.json'));
+let bad = JSON.parse(fs.readFileSync('./src/toxic/bad.json'));
+// Database Rpg
+ let _limit = JSON.parse(fs.readFileSync('./storage/user/limit.json'));
+ let _buruan = JSON.parse(fs.readFileSync('./storage/user/bounty.json'));
+ let _darahOrg = JSON.parse(fs.readFileSync('./storage/user/blood.json'))
+
+//read database using mongodb and lowdb by xeon
+global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+global.db = new Low(
+  /https?:\/\//.test(opts['db'] || '') ?
+    new cloudDBAdapter(opts['db']) : /mongodb/.test(opts['db']) ?
+      new mongoDB(opts['db']) :
+      new JSONFile(`src/database.json`)
+)
+global.DATABASE = global.db // Backwards Compatibility
+global.loadDatabase = async function loadDatabase() {
+  if (global.db.READ) return new Promise((resolve) => setInterval(function () { (!global.db.READ ? (clearInterval(this), resolve(global.db.data == null ? global.loadDatabase() : global.db.data)) : null) }, 1 * 1000))
+  if (global.db.data !== null) return
+  global.db.READ = true
+  await global.db.read()
+  global.db.READ = false
